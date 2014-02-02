cf5e3801f2b24dfc7a64e4a4929239c0631f0d75
Martin Thoresen <martinhath@gmail.com>
2014-02-02 17:39:02 +0100

    Started on preferences.

---
+++ Barteguiden/src/main/java/com/underdusken/kulturekalendar/ui/fragments/PrefsFragment.java
@@ -0,0 +1,17 @@
+package com.underdusken.kulturekalendar.ui.fragments;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.underdusken.kulturekalendar.R;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class PrefsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
