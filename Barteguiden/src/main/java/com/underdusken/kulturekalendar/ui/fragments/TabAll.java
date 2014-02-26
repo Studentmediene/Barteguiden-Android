756cb850e7f8cd5b3c7113c0f8631979b2692aa6
Martin Thoresen <martinhath@gmail.com>
2014-02-26 21:08:41 +0100

    Issue#3
    Started. Items does show, but the view for empty list won't.

--- Barteguiden/src/main/java/com/underdusken/kulturekalendar/ui/fragments/TabAll.java
+++ Barteguiden/src/main/java/com/underdusken/kulturekalendar/ui/fragments/TabAll.java
@@ -62,7 +62,7 @@ public class TabAll extends Fragment implements SearchView.OnQueryTextListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_all_events, container, false);
+        return inflater.inflate(R.layout.tab_all, container, false);
 
     }
 
